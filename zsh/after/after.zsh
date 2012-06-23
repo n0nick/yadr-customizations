bf37698bf68db817241683158e5a47a35c2a0e35
Sagie Maoz <sagie@maoz.info>
2012-06-23 22:09:09 +0300

    Adding my current vim and zsh customizations.

---
+++ zsh/after/after.zsh
@@ -0,0 +1,24 @@
+#zstyle ':omz:module:prompt' theme 'minimal'
+#autoload -Uz promptinit
+#promptinit
+#prompt minimal
+
+# define your own aliases or override those provided by YADR.
+# alias ls='ls -lAhFG'
+alias ls='ls -Fh --color=auto'
+alias hosts='sudo vim /private/etc/hosts'
+
+# Automatically cd to frequently used directories http://robots.thoughtbot.com/post/10849086566/cding-to-frequently-used-directories-in-zsh
+setopt auto_cd
+cdpath=($HOME/dev)
+
+# Fancy globbing http://linuxshellaccount.blogspot.com/2008/07/fancy-globbing-with-zsh-on-linux-and.html
+setopt extendedglob
+
+# Set up fasd tab completion
+eval "$(fasd --init auto)"
+
+# Some fasd aliases
+alias j="fasd_cd -d" # 'j'ump makes more sense than 'z'
+alias o='a -e open'
+
---
+++ zsh/before/before.zsh
@@ -0,0 +1,5 @@
+# append your own plugins. the $plugins at the end includes the plugins
+# defined by YADR.
+plugins=(brew gist osx rails3 taskwarrior vagrant $plugins)
+
+
